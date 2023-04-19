4fa0438ed550cd1200755185c0d0f66707f9bd45
Giovanny Cordeiro <giovannycordeiropb@gmail.com>
2023-04-19 20:29:55 -0300

    Change to environment variables

--- src/database.ts
+++ src/database.ts
@@ -5,20 +5,19 @@ import Customer from "./models/customer";
 dotenv.config();
 
 const nameDB = `${process.env.NAMEDB}`;
-const dialect = `${process.env.DIALECTDB}`;
 const username = `${process.env.USERNAMEDB}`;
 const password = `${process.env.PASSWORDDB}`;
 const host = `${process.env.HOSTDB}`;
-const port = process.env.PORTDB;
+const port = `${process.env.PORTDB}`;
 
 
 const database = new Sequelize({
-  database: 'crud',
+  database: nameDB,
   dialect: 'mysql',
-  username: 'root',
-  password: 'gio434445',
-  host: 'localhost',
-  port: 3306,
+  username: username,
+  password: password,
+  host: host,
+  port: parseInt(port),
 })
 
 database.addModels([Customer]);
