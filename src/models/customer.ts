2d56163bd955feb899a737dbf72948e8495a4fb9
Giovanny Cordeiro <giovannycordeiropb@gmail.com>
2023-04-19 17:31:22 -0300

    Removing defining wrong way customer table

--- src/models/customer.ts
+++ src/models/customer.ts
@@ -1,38 +1,4 @@
-import { Model, DataType, Table, Column, PrimaryKey, AutoIncrement} from "sequelize-typescript";
-import database from "../database";
-
-// interface CustomerAttributes{
-//   id: number;
-//   first_name: string;
-//   last_name: string;
-//   CPF: number;
-// }
-// class Customer extends Model<CustomerAttributes> implements CustomerAttributes{
-//   public id!:number;
-//   public first_name!: string;
-//   public last_name!:string;
-//   public CPF!:number;
-// }
-
-// Customer.init({
-//   id:{
-//     type: DataType.INTEGER.UNSIGNED,
-//     autoIncrement: true,
-//     primaryKey: true
-//   },
-//   first_name:{
-//     type: DataType.STRING(50)
-//   },
-//   last_name:{
-//     type:DataType.STRING(50)
-//   },
-//   CPF:{
-//     type: DataType.INTEGER,
-//     unique:true
-//   }
-// }, {tableName: 'people', sequelize: database}
-// );
-
+import { Model, Table, Column, PrimaryKey, AutoIncrement} from "sequelize-typescript";
 @Table
 class Customer extends Model{
   @PrimaryKey
